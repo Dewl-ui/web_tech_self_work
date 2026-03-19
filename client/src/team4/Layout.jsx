5c06a9318613750178ed4669e7d365dce1ac9e1a
B241940029 <b241940029@gmail.com>
2026-03-19 22:03:53 +0800

    refactor: remove unused components and implement Login page

--- client/src/team4/Layout.jsx
+++ client/src/team4/Layout.jsx
@@ -1,9 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import SideMenu from "./SideMenu";
 
 const Layout = () => {
   return <div className='w-full flex-row gap-4 md:flex'>
-    <SideMenu />
     <Outlet />
   </div>
 };
---
+++ client/src/team4/pages/login/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <div className="w-full min-h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold">Login Page - First Version</h1>
+        </div>
+    );
+}
