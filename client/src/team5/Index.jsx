9c5f3950397be65d14a48f98e3bdfcb8ce68674a
Oyumaa Chimeddagva <feelingjasmine@gmail.com>
2026-04-01 23:58:35 +0800

    team5: question component-уудыг pages хавтас руу шилжүүлэв + Dashboard.jsx замчлал нэмэв

--- client/src/team5/Index.jsx
+++ client/src/team5/Index.jsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+
+// Layout & Common
 import Layout from "./Layout";
 import Home from "./Home";
 import Example from "./Example";
+
+// Question Bank Pages
 import QuestionTypes from "./pages/QuestionTypes";
 import QuestionLevels from "./pages/QuestionLevels";
 import QuestionList from "./pages/QuestionList";
@@ -11,16 +15,22 @@ import QuestionView from "./pages/QuestionView";
 import QuestionEdit from "./pages/QuestionEdit";
 import QuestionReport from "./pages/QuestionReport";
 
+// Dashboard
+import Dashboard from "./pages/Dashboard";     // ← Зөв import
+
 const Index = () => {
   return (
     <Routes>
       <Route path="" element={<Layout />}>
+        {/* Home */}
         <Route index element={<Home />} />
         <Route path="example" element={<Example />} />
 
+        {/* Question Bank Routes */}
         <Route path="question-types" element={<QuestionTypes />} />
         <Route path="question-levels" element={<QuestionLevels />} />
 
+        {/* Courses + Questions Nested Routes */}
         <Route path="courses/:course_id/questions">
           <Route index element={<QuestionList />} />
           <Route path="create" element={<QuestionCreate />} />
@@ -30,6 +40,9 @@ const Index = () => {
         </Route>
 
         <Route path="courses/:course_id/question-points" element={<QuestionPoints />} />
+
+        {/* Dashboard */}
+        <Route path="dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
   );
