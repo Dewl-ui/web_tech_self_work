ee8b3578fd25ea11f76e62c884da76a203703449
“Shinebayar” <xdabnismoffical@gmail.com>
2026-04-08 21:47:02 +0800

    Index.jsx page uud nemej uguv

--- client/src/team6/Index.jsx
+++ client/src/team6/Index.jsx
@@ -1,14 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Layout";
-import Home from "./Home";
-import Example from "./Example";
+import DashboardPage from "./DashboardPage";
+import CoursesPage from "./CoursesPage";
+import CourseDetailPage from "./CourseDetailPage";
+import ExamsPage from "./ExamsPage";
+import ExamDetailPage from "./ExamDetailPage";
 
 const Index = () => {
   return (
     <Routes>
-      <Route path="" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path='example' element={<Example />} />
+      <Route element={<Layout />}>
+        <Route index element={<Navigate to="dashboard" replace />} />
+        <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="courses" element={<CoursesPage />} />
+        <Route path="courses/:courseId" element={<CourseDetailPage />} />
+        <Route path="courses/:courseId/exams" element={<ExamsPage />} />
+        <Route path="exams/:examId" element={<ExamDetailPage />} />
       </Route>
     </Routes>
   );
